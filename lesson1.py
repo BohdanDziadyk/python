0bd2f715eb2a4341374d17c0b60e7bb905267f1f
BohdanDziadyk <bohdan.dziadyk1@gmail.com>
2020-08-20 19:26:59 +0300

    mult table comp square comp

--- lesson1.py
+++ lesson1.py
@@ -1,11 +1,13 @@
 list = [22, 3, 5, 2, 8, 2, -23, 8, 23, 5]
-i = 1;
+i = 1
 while i > 0:
     print(list)
     print("1) min of list")
     print("2) delete similar")
     print("3) replace every 4th by X")
     print("4) print closest number to lists avg")
+    print("5) print square with custom side")
+    print("6) print tablichky umnozhenia")
     print("type 0 to end")
     inp = int(input("Choose an option:"))
     if inp == 1:
@@ -49,5 +51,40 @@ while i > 0:
                 print(item)
                 print("-------------------------------------------------------------------")
                 break
+    elif inp == 5:
+        inp1 = int(input("Enter square side:"))
+        x = 0
+        print("-------------------------------------------------------------------")
+        while x < inp1:
+            print("*", end="")
+            x += 1
+        print("")
+        x = 0
+        while x < inp1 - 2:
+            print("*", end="")
+            y = 0
+            while y < inp1 - 2:
+                print(" ", end="")
+                y += 1
+            print("*")
+            x += 1
+        x = 0
+        while x < inp1:
+            print("*", end="")
+            x += 1
+        print("")
+        print("-------------------------------------------------------------------")
+    elif inp == 6:
+        x = 1
+        y = 1
+        while x < 10:
+            while y < 10:
+                print(x * y, end="")
+                print(" ", end="")
+                y += 1
+            print("")
+            y = 1
+            x += 1
+        print("")
     elif inp == 0:
         break
